76eb50c145b12c6fbf03dcf98874dd007567479a
tarsir <stephen.hara@stephenhara.com>
2021-04-07 20:23:04 +0900

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 # Some stuff I wanna run on all of my *nix systems
 
 echo "Installing basic packages"
-sudo apt update && sudo apt install unzip make libssl-dev libncurses5-dev gcc automake autoconf
+sudo apt update && sudo apt install unzip make libssl-dev libncurses5-dev gcc automake autoconf libreadline-dev zlib1g-dev
 
 echo "Installing asdf"
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
+if [ -e "~/.asdf" ]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+    cd ~/.asdf
+    git checkout "$(git describe --abbrev=0 --tags)"
+fi
 
 . "$HOME/.asdf/asdf.sh"
 . "$HOME/.asdf/completions/asdf.bash"
@@ -19,12 +21,15 @@ for plugin in "elixir" "erlang" "postgres"; do
     echo "Installing plugin $plugin"
     asdf plugin add "$plugin"
     asdf install "$plugin" latest
+    asdf global "$plugin" $(asdf latest "$plugin")
 done
 
 echo "asdf plugins installed!"
 echo
 
-echo "Installing rustup"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if [ -e "~/.rustup" ]; then
+    echo "Installing rustup"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
 
 echo "Done!"
