33e9e3fb6c26bdbe1a7c79d8708ef318a2a09bbc
tarsir <stephen.hara@stephenhara.com>
2021-04-22 22:30:26 +0900

    merge nvim config

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Backing up existing files to this directory just in case"
-FILES=(".bashrc" ".dockerbash" ".gitbash" ".promptbash" ".vimrc")
+FILES=(".bashrc" ".dockerbash" ".gitbash" ".promptbash" "nvim.vim")
 mkdir -p backups
 for f in ${FILES[*]}; do
     cp $f backups/backup$f
@@ -12,13 +12,5 @@ for f in ${FILES[*]}; do
     rm ~/$f
     ln -s $(pwd)/${f} $HOME/${f}
 done
-
-# ln -s $(pwd)/.dockerbash $HOME/.dockerbash
-# ln -s $(pwd)/.gitbash $HOME/.gitbash
-# ln -s $(pwd)/.promptbash $HOME/.promptbash
-# ln -s $(pwd)/.venvbash $HOME/.venvbash
-# ln -s $(pwd)/.bashrc $HOME/.bashrc
-# ln -s $(pwd)/.bashrc $HOME/.bashrc
-# ln -s $(pwd)/.vimrc $HOME/.vimrc
 
 echo "Done!"
