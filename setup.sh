9de70eae52dc237502ba1a5aebea2fe102234890
Stephen Hara <stephen.hara@stephenhara.com>
2022-07-16 00:07:27 +0900

    fix some things with nvim install on WSL and git

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/downloads/AppImages
 
 echo "Installing basic packages"
-if [ ! curl --help ]; then
-    sudo apt update && sudo apt install curl unzip make libssl-dev libncurses5-dev gcc automake autoconf libreadline-dev zlib1g-dev g++ silversearcher-ag inotify-tools libfuse2 setuptools
+if ! curl --help ; then
+    sudo apt update && sudo apt install curl unzip make libssl-dev libncurses5-dev gcc automake autoconf libreadline-dev zlib1g-dev g++ silversearcher-ag inotify-tools libfuse2 python-setuptools
 fi
 
 echo "Installing asdf"
@@ -21,11 +21,15 @@ fi
 echo "Installing asdf plugins"
 echo
 
-for plugin in "elixir" "erlang" "postgres" "python"; do
-    echo "Installing plugin $plugin"
+for plugin in "elixir" "erlang" "postgres" "python" "neovim"; do
+  if ! asdf current $plugin; then
+    echo "Adding plugin $plugin"
     asdf plugin add "$plugin"
     asdf install "$plugin" latest
     asdf global "$plugin" $(asdf latest "$plugin")
+  else
+    echo "Skipping plugin $plugin"
+  fi
 done
 
 if ! asdf current nodejs; then
@@ -44,17 +48,28 @@ echo
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
+NVIM_FILENAME="nvim.appimage"
+NVIM_LOCATION="~/downloads/AppImages/${NVIM_FILENAME}"
+
+# does not work on WSL, use asdf instead
 if [ ! -s "$NVIM_LOCATION" ]; then
+  if ! uname -r | grep WSL2; then
+    pushd .
     echo "Installing nvim"
     NVIM_FILENAME="nvim.appimage"
-    NVIM_LOCATION="~/downloads/AppImages/${NVIM_FILENAME}"
-    curl -L "https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage" -o ${NVIM_LOCATION}
+    NVIM_LOCATION="$HOME/downloads/AppImages/${NVIM_FILENAME}"
+    curl -L "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage" -o ${NVIM_LOCATION}
     chmod u+x "$HOME/downloads/AppImages/${NVIM_FILENAME}"
+    cd $(dirname "${NVIM_LOCATION}")
+    ./${NVIM_FILENAME} --appimage-extract
+    sudo ln -s $(pwd)/squashfs-root/AppRun /usr/bin/nvim
+    popd
+  fi
 fi
 
 echo
 
-if [ ! -e "~/.rustup" ]; then
+if [ ! -d "$HOME/.rustup/" ]; then
     echo "Installing rustup"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
