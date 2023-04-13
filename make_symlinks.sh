48c2ef433aeca80e4ac23fe0df913284ba5556d2
Stephen Hara <stephen.hara@stephenhara.com>
2023-04-14 01:23:00 +0900

    Add configs for COC and gitui

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -20,15 +20,24 @@ rm -f "${ssh_config_path}"
 echo "Copying ./ssh_config to ${ssh_config_path}"
 cp "./ssh_config" "${ssh_config_path}"
 
+rm -f "$HOME/.config/gitui/key_bindings.ron" "$HOME/.config/nvim/coc-settings.json"
+cp "./key_bindings.ron" "$HOME/.config/gitui/key_bindings.ron"
+cp "./coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
+
 # Do separately for nvim
 
 echo "Now copying nvim config"
 
-NVIM_CONFIG="nvim.vim"
-NVIM_TARGET_PATH="$HOME/.config/nvim/init.vim"
+NVIM_CONFIG="nvim.lua"
+NVIM_PLUGINS_CONFIG="nvim_plugins.lua"
+NVIM_BASE_DIR="$HOME/.config/nvim/"
+NVIM_TARGET_PATH="${NVIM_BASE_DIR}/${NVIM_CONFIG}"
 mkdir -p $(dirname $NVIM_TARGET_PATH)
-cp "${NVIM_CONFIG}" "backups/backup${NVIM_CONFIG}-${DATE}"
-rm -f "${NVIM_TARGET_PATH}"
-ln -s "$(pwd)/${NVIM_CONFIG}" "${NVIM_TARGET_PATH}"
+mkdir -p "${NVIM_BASE_DIR}/lua"
+cp "${NVIM_BASE_DIR}/init.lua" "backups/backup${NVIM_CONFIG}-${DATE}"
+cp "${NVIM_BASE_DIR}/lua/plugins.lua" "backups/backup${NVIM_PLUGINS_CONFIG}-${DATE}"
+rm -f "${NVIM_BASE_DIR}/init.lua" "${NVIM_BASE_DIR}/lua/plugins.lua"
+ln -s "$(pwd)/${NVIM_CONFIG}" "${NVIM_BASE_DIR}/init.lua"
+ln -s "$(pwd)/${NVIM_PLUGINS_CONFIG}" "${NVIM_BASE_DIR}/lua/plugins.lua"
 
 echo "Done!"
