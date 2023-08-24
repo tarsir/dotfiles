ab2deb5343cb6f8a535875d41ef115ceb7dc2441
Stephen Hara <stephen.hara@stephenhara.com>
2023-08-24 23:54:42 +0900

    Add completion border and disable completion for markdown

--- nvim_configs/remap.lua
+++ nvim_configs/remap.lua
@@ -52,7 +52,6 @@ vim.keymap.set("n", "g]", vim.diagnostic.goto_next, keymap_opts)
 
 local format_sync_grp = vim.api.nvim_create_augroup("Format", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.rs",
   callback = function()
     vim.lsp.buf.format({ timeout_ms = 200 })
   end,
