4515b528c4a460b79b3361125eea3521da7e356c
Stephen Hara <stephen.hara@stephenhara.com>
2025-01-09 23:00:25 +0900

    nvim config updates

--- nvim_configs/set.lua
+++ nvim_configs/set.lua
@@ -7,8 +7,8 @@ vim.opt.syntax = "ON"
 vim.o.termguicolors = true
 vim.o.foldenable = false
 
-vim.cmd.highlight('ColorColumn ctermbg=magenta')
-vim.opt.colorcolumn = { '+1', 100 }
+vim.cmd.highlight("ColorColumn ctermbg=magenta")
+vim.opt.colorcolumn = { "+1", 100 }
 
 -- tabwidths and such
 vim.opt.shiftwidth = 2
@@ -22,13 +22,13 @@ vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 -- Always show the signcolumn, otherwise it would shift the text each time
 -- diagnostics appear/become resolved.
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 -- fix some elixir file associations
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.ex", "*.exs", "mix.lock" },
-  command = "set filetype=elixir"
-})
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.eex", "*.heex", "*.leex", "*.sface", "*.lexs" },
-  command = "set filetype=eelixir"
-})
+-- vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+--   pattern = { "*.ex", "*.exs", "mix.lock" },
+--   command = "set filetype=elixir"
+-- })
+-- vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+--   pattern = { "*.eex", "*.heex", "*.leex", "*.sface", "*.lexs" },
+--   command = "set filetype=eelixir"
+-- })
