8d4c64eef9eb78c0d8ef9f25cacd2c49858a69bd
Stephen Hara <stephen.hara@stephenhara.com>
2025-06-21 17:07:23 +0900

    make nvim config a symlink

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -38,26 +38,10 @@ for f in ${NUSHELL_FILES[*]}; do
   ln -s "$(pwd)/${f}" "$NUSHELL_DIR/${f}"
 done
 
-# Do separately for nvim
-
 echo "Now copying nvim config"
-NVIM_CONFIG="nvim.lua"
-NVIM_PLUGINS_PATH="lua/plugins"
-NVIM_BASE_DIR="$HOME/.config/nvim/"
-NVIM_TARGET_PATH="${NVIM_BASE_DIR}/${NVIM_CONFIG}"
-cp "${NVIM_BASE_DIR}/init.lua" "backups/backup${NVIM_CONFIG}-${DATE}"
-rm -rf "${NVIM_BASE_DIR}/init.lua" "${NVIM_BASE_DIR}/lua"
-et "${NVIM_BASE_DIR}"
-mkdir -p $(dirname $NVIM_TARGET_PATH)
-mkdir -p "${NVIM_BASE_DIR}/${NVIM_PLUGINS_PATH}"
-ln -s "$(pwd)/${NVIM_CONFIG}" "${NVIM_BASE_DIR}/init.lua"
-for f in $(rg --files nvim_configs); do
-  trimmed=$(printf '%s\n' $f | cut -f2- -d'/')
-  final_path="${NVIM_BASE_DIR}/lua/${trimmed}"
-  mkdir -p $(dirname $final_path)
-  cp "$f" "$final_path"
-done
-
+NVIM_BASE_DIR="$HOME/.config/nvim"
+rm -r "${NVIM_BASE_DIR}"
+ln -s "$(pwd)/nvim_configs" "${NVIM_BASE_DIR}"
 et "${NVIM_BASE_DIR}"
 
 echo "Done!"
